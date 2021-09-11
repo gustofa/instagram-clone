0146a7dcca60001396ec330e568778232a4a1bb2
Gustavo <gustofa@gmail.com>
2021-09-11 00:15:35 -0300

    Adding Firebase and getting posts from it

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react"; //
 import "./App.css";
 import Post from "./Post";
 
+import { db } from "./firebase.js";
+// eslint-disable-next-line
+import { collection, query, onSnapshot } from "@firebase/firestore";
+
 function App() {
   const [posts, setPosts] = useState([
-    {
-      userName: "User 6",
+    /*{
+      userName: "User 5",
       caption: "I LOVE JAPAN",
       imageUrl:
         "https://images.pexels.com/photos/3408353/pexels-photo-3408353.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
-    },
+    } 
     {
       userName: "User 5",
       caption: "I LOVE JAPAN TOO",
@@ -27,8 +31,27 @@ function App() {
       caption: "I REALLY LOVE JAPAN",
       imageUrl:
         "https://images.pexels.com/photos/5745029/pexels-photo-5745029.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
-    },
+    },*/
   ]);
+
+  useEffect(() => {
+    /*    (async function () {
+      try {
+        const postsCol = collection(db, "posts");
+        console.log(postsCol);
+        const postsSnapshot = await getDocs(postsCol);
+        setPosts(postsSnapshot.docs.map((doc) => doc.data()));
+      } catch (error) {
+        console.log("Error fetching data from Firebase", error);
+      }
+    })();
+*/
+
+    const postsCol = query(collection(db, "posts"));
+    onSnapshot(postsCol, function (querysnapshot) {
+      setPosts(querysnapshot.docs.map((doc) => doc.data()));
+    });
+  }, []);
 
   return (
     <div className="App">
---
+++ src/firebase.js
@@ -0,0 +1,29 @@
+//import firebase from "firebase/compat/app";
+//import "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBd-DBj5QtIC2TuQ2bZaLkmpvj2l72zxQM",
+  authDomain: "instagram-clone-58c31.firebaseapp.com",
+  projectId: "instagram-clone-58c31",
+  storageBucket: "instagram-clone-58c31.appspot.com",
+  messagingSenderId: "444079936563",
+  appId: "1:444079936563:web:b66e7acda6e0e39044bf46",
+  measurementId: "G-0K52G22KP2",
+};
+
+//firebase.initializeApp(firebaseConfig);
+//const db = firebase.firestore();
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+/* const auth = firebase.auth();
+const storage = firebase.storage(); */
+//const auth = firebaseApp.auth();
+//const storage = firebaseApp.storage();
+
+console.log(firebaseApp);
+
+export { db }; //, auth, storage };
